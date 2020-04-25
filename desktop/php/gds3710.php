4dbf94848ca92288da2113870ce7a44a853203a0
Richard Perez <richard@jeedom>
2020-04-25 23:47:10 +0200

    Adding SIP debug possibility

--- desktop/php/gds3710.php
+++ desktop/php/gds3710.php
@@ -23,79 +23,89 @@
        </div>
    </div>
 
-   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+  <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
     <legend>{{Mes GDS 3710}}</legend>
-  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-  <div class="eqLogicThumbnailContainer">
+    <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+    
+    <div class="eqLogicThumbnailContainer">
+
       <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
         <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
         <br>
         <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
-    </div>
+      </div>
+
       <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-      <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
-    <br>
-    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+        <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+        <br>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+      </div>
+
+    </div>
+
+    <legend><i class="fa fa-table"></i> {{Liste des équipements}}</legend>
+    
+    <div class="eqLogicThumbnailContainer">
+
+      <?php
+        foreach ($eqLogics as $eqLogic) {
+          $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+          echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+          echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
+          echo "<br>";
+          echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
+          echo '</div>';
+        }
+      ?>
+
+    </div>
   </div>
-  </div>
-  <legend><i class="fa fa-table"></i> {{Liste des équipements}}</legend>
-<div class="eqLogicThumbnailContainer">
 
-<?php
-  foreach ($eqLogics as $eqLogic) {
-    $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-    echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
-    echo "<br>";
-    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
-    echo '</div>';
-  }
-?>
-</div>
-</div>
+  <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+    <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+    <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+      <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Configuration}}</a></li>
+      <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
 
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-  <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Configuration}}</a></li>
-    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+      <?php
+        function array_unique_multidimensional($input)
+        {
+            $serialized = array_map('serialize', $input);
+            $unique = array_unique($serialized);
+            return array_intersect_key($input, $unique);
+        }
 
-    <?php
-      function array_unique_multidimensional($input)
-      {
-          $serialized = array_map('serialize', $input);
-          $unique = array_unique($serialized);
-          return array_intersect_key($input, $unique);
-      }
+        $gds3710EventList = gds3710::get_GDS3710_event_list();
+        $stack_section = array(); 
 
-      $gds3710EventList = gds3710::get_GDS3710_event_list();
-      $stack_section = array(); 
-      foreach ($gds3710EventList as $row) {
-        array_push($stack_section, array('section'=> $row['section'],'section_icon' => $row['section_icon']));
-      }
-      $stack_section = array_unique_multidimensional($stack_section);
-      $output = '';
-      //print_r($stack_section);
-      foreach ($stack_section as $row_main) {
-        ///echo('test');
-        $output = $output.'<li role="presentation"><a href="#'.str_replace(' ', '',strtolower($row_main['section'])).'actiontab" aria-controls="profile" role="tab" data-toggle="tab"><i class="'.$row_main['section_icon'].'"></i> {{'.ucfirst($row_main['section']).'}}</a></li>';
-      }
-      echo($output);
-    ?>
+        foreach ($gds3710EventList as $row) {
+          array_push($stack_section, array('section'=> $row['section'],'section_icon' => $row['section_icon']));
+        }
 
-  </ul>
+        $stack_section = array_unique_multidimensional($stack_section);
+        $output = '';
+
+        foreach ($stack_section as $row_main) {
+          $output = $output.'<li role="presentation"><a href="#'.str_replace(' ', '',strtolower($row_main['section'])).'actiontab" aria-controls="profile" role="tab" data-toggle="tab"><i class="'.$row_main['section_icon'].'"></i> {{'.ucfirst($row_main['section']).'}}</a></li>';
+        }
+
+        echo($output);
+      ?>
+
+    </ul>
+
   <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-  	<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-     	<br/>
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+    <br/>
   		<form class="form-horizontal">
         	<fieldset>
             	<div class="form-group">
                 	<label class="col-sm-3 control-label">{{Nom du GDS3710}}</label>
 	                <div class="col-sm-3">
-	                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+	                 <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
 	                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
 	                </div>
             	</div>
@@ -213,6 +223,13 @@
             <label class="col-sm-3 control-label">{{URI du portier SIP :}}</label>
             <div class="col-sm-3">
                 <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="portier_sip_uri" placeholder="URI du portier SIP"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-3 control-label">{{Débogage SIP :}}</label>
+            <div class="col-sm-3">
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="is_sip_debug_enabled" checked/>{{Activer}}</label>
             </div>
         </div>
 
